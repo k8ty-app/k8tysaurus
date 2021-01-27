ca0cca365d8c95e52b827f4a12333823ac05ab10
Mark Rudolph <mark@k8ty.app>
2021-01-26 22:50:32 -0500

    get Buffer working

--- k8tysaurus/src/theme/Root.js
+++ k8tysaurus/src/theme/Root.js
@@ -2,6 +2,8 @@ import React from 'react';
 import firebase from 'firebase/app';
 import config from '../config/config';
 
+global.Buffer = global.Buffer || require('buffer').Buffer;
+
 const fbc = JSON.parse(
     Buffer.from(config.FIREBASE_CONFIG, 'base64').toString()
 );
